6ebeeb5f1a733404fd3dcb8753767717ca0962a1
Felipe  Fahrion <felipefahrion@MacBook-Air-de-Felipe.local>
2022-03-16 14:54:06 -0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,33 @@
+var protoLoader = require("@grpc/proto-loader");
+const grpc = require("@grpc/grpc-js");
+const news = require('./news')
+
+const PROTO_PATH = "./news.proto";
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const newsProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+
+server.addService(newsProto.NewsService.service, {
+  getAllNews: (_, callback) => {
+    callback(null, news);
+  },
+});
+
+server.bindAsync(
+  "127.0.0.1:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    console.log("Server running at http://127.0.0.1:50051");
+    server.start();
+  }
+);
