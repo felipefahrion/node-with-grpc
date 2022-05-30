6ae0a2d23dd89c432ffa731e2f7e4772bafa93b1
Felipe  Fahrion <felipefahrion@MacBook-Air-de-Felipe.local>
2022-05-30 12:25:09 -0300

    implemented rest api

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,26 +1,58 @@
 const express = require('express');
-const news = require('../../db/news')
+const newsList = require('../../db/news')
 
 const app = express();
+app.use(express.json())
 
 const PORT = 3000
 
 app.get('/getAll', (req, res) => {
+  return res.send(newsList);
+});
+
+app.get('/getById/:id', (req, res) => {
+  const { id } = req.params
+
+  const news = newsList.news.find((e) => e.id === id)
+
+  if (!news) return res.send(new Error('Not found'))
+
   return res.send(news);
 });
 
-app.get('/getById', (req, res) => {
-  return res.send(news);
-});
+app.delete('/delete/:id', (req, res) => {
+  const { id } = req.params
 
-app.delete('/delete', (req, res) => {
-  return res.send(news);
+  const newsIndex = newsList.news.findIndex((e) => e.id === id)
+
+  if (newsIndex > -1) {
+    newsList.news.splice(newsIndex, 1);
+    return res.send(newsList);
+  }
+
+  return res.send(new Error('Not found'))
 });
 
 app.post('/create', (req, res) => {
-  return res.send(news);
+
+  const {title, body, postImage} = req.body
+
+  const news = newsList.news.find((e) => e.title === title)
+    
+  if (news) return res.send(new Error('Not found'), null)
+
+  const newNews = {
+    id: (newsList.news.length + 1).toString(),
+    title,
+    body,
+    postImage
+  }
+
+  newsList.news.push(newNews)
+
+  return res.send(newsList);
 });
 
 app.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`),
+  console.log(`Server running on port ${PORT}!`),
 );
